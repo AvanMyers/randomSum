de669eb0aaa74e45f880ff4776611e806797bf6e
AvanMyers <smail1341@gmail.com>
2022-08-14 15:57:41 +0300

    chanje

--- randomSum/randomSum/Program.cs
+++ randomSum/randomSum/Program.cs
@@ -23,15 +23,12 @@ namespace randomSum
 
             for (int i = 0; i < randomNumber; i++)
             {
-                receivedFirstNumber = i % firstMultiple;
-                receivedSecondNumber = i % secondMultitipe;
-
-                if (receivedFirstNumber == 0 || receivedSecondNumber == 0)
+                if (i % firstMultiple == 0 || i % secondMultitipe == 0)
                 {
                     numberForSum += i;
                 }
                
-                Console.WriteLine($"{i}.Сумма чисел кратных 3 или 5: {numberForSum}");
+                Console.WriteLine($"{i}.Сумма чисел кратных {firstMultiple} или {secondMultitipe}: {numberForSum}");
             }
             Console.ReadKey();
         }
