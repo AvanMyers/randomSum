1ddffc5fcdfefc3e21d6c60f1d3f40300a6d0a86
AvanMyers <smail1341@gmail.com>
2022-08-14 14:23:32 +0300

    sum

---
+++ randomSum/randomSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNumber;
+            int numberForSum = 0;
+            int numberOfRepeat = 10;
+            int multiple = 3;
+            int receivedNumber;
+
+            for (int i = 0; i < numberOfRepeat; i++)
+            {
+                randomNumber = rand.Next(1, 100);
+                receivedNumber = randomNumber % multiple;
+
+                if (receivedNumber  == 0)
+                {
+                    numberForSum += randomNumber;
+                }
+                Console.WriteLine($"Случайное число: {randomNumber}");
+            }
+            Console.WriteLine(numberForSum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ randomSum/randomSum/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("randomSum")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("randomSum")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("cc940926-f909-419b-a1a5-1313f2e7dbb7")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
